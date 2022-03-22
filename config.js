7ff8ad34415642471a0645f1189550aacdfa5420
github.com/ingoradatz <560460+ingoradatz@users.noreply.github.com>
2022-03-22 13:09:14 +0100

    handled the CNAME file correctly

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPassthroughCopy("input/CNAME")
   return {
     dir: {
       input: "input",
